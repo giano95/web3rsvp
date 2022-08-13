6d80fbc52f0e6c6441b14be1e09942f186bebe31
Marco Gianelli <marcogianelli@192.168.1.79>
2022-08-13 04:39:22 +0200

    added deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,18 @@
 require("@nomiclabs/hardhat-ethers")
+require("@nomiclabs/hardhat-waffle") //new update from hardhat requires to import 'hardhat-toolbox'
+require("dotenv").config()
 
 module.exports = {
     solidity: "0.8.4",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        mumbai: {
+            url: process.env.MUMBAI_RPC_URL || "",
+            accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+            gas: 2100000,
+            gasPrice: 8000000000,
+        },
+    },
 }
